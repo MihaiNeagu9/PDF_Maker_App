03ae1ffeea5c03347d1a395ae3a948b6ac84ae7f
MihaiNeagu9 <mihaineagu2001@yahoo.com>
2025-08-28 09:42:34 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import fpdf
+import pandas
+
+pdf = fpdf.FPDF(orientation="P", unit="mm", format="A4")
+df = pandas.read_csv("003 topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], border=0, ln=1, align="L")
+    pdf.line(10,21, 200, 21)
+
+
+pdf.output("output.pdf")
